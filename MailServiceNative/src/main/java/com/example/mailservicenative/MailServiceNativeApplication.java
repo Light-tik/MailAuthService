f5a9958f8dd8621d9fe74156105e867c9a967b96
Svetlana <kuzminovasveta19@gmail.com>
2025-09-18 13:08:35 +0300

    Created MailServiceNative

---
+++ MailServiceNative/src/main/java/com/example/mailservicenative/MailServiceNativeApplication.java
@@ -0,0 +1,12 @@
+package com.example.mailservicenative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailServiceNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailServiceNativeApplication.class, args);
+    }
+}
---
+++ MailServiceNative/src/test/java/com/example/mailservicenative/MailServiceNativeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.mailservicenative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
